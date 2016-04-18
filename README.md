5a96deec27c9a320e3a4e09f0fd9a32f0a61cd6b
Cédric <cedced19@gmail.com>
2016-04-18 19:11:48 +0200

    Update test and fill README.md

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 [![Build Status](https://travis-ci.org/cedced19/learn-memory-statistics.svg?branch=master)](https://travis-ci.org/cedced19/learn-memory-statistics)
 
 A module to get statistics from data from a Learn Memory website.
+
+```bash
+npm install --save learn-memory-statistics
+```
+
+## Example:
+
+```javascript
+var statistics = require('learn-memory-statistics');
+var request = require('request');
+
+request('https://cedced19.github.io/learn-memory-static/data.json', function (error, response, body) {
+        var data = JSON.parse(body);
+	      console.log(statistics(data));
+});
+```
--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 var statistics = require('../');
 var request = require('request');
+var expect = require('chai').expect;
 
 
 describe('Test receving statisctics from a list of lessons', function () {
@@ -12,6 +13,21 @@ describe('Test receving statisctics from a list of lessons', function () {
   });
 
   it('should give statisctics for a list of lessons', function () {
-    //console.log(require('../')(data));
+    var result = statistics(data);
+    expect(result).to.be.an('object');
+    expect(result.languages).to.be.an('object');
+    expect(result.subjects).to.be.an('object');
+    expect(result.createdDate).to.be.an('object');
+    expect(result.createdDate.years).to.be.an('object');
+    expect(result.createdDate.months).to.be.an('object');
+    expect(result.createdDate.hours).to.be.an('object');
+    expect(result.createdDate.days).to.be.an('object');
+    expect(result.updatedDate).to.be.an('object');
+    expect(result.updatedDate.years).to.be.an('object');
+    expect(result.updatedDate.months).to.be.an('object');
+    expect(result.updatedDate.hours).to.be.an('object');
+    expect(result.updatedDate.days).to.be.an('object');
+    expect(result.years).to.be.an('object');
+    //console.log(result);
   });
 });
